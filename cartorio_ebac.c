8b77d40a1bc57f0d8df83f7e30a62d237bcd48e4
Wendel-5988 <153458306+Wendel-5988@users.noreply.github.com>
2023-12-10 19:43:42 -0300

    Deletar, Saída e Edição!
    Incluso a opção de Deletar, e com ela a confirmação de exclusão; Opção de saída do programa; Opção de editar arquivos já existentes.

--- cartorio_ebac.c
+++ cartorio_ebac.c
@@ -5,94 +5,142 @@
 
 int registro(){
 	setlocale (LC_ALL, "Portuguese");
-	
+	  // Inicio da criação de variáveis/strings.
 	char arquivo[40];
 	char cpf[40];
 	char nome[40];
 	char sobrenome[40];
 	char cargo[40];
-	
+	int opcao = 0;
+	int registrar(){
+			FILE *file;  //Cria um arquivo
+			file = fopen(arquivo, "w");  //fopen abre o arquivo. "w" serve para criar um novo arquivo.
+			fprintf(file, "CPF: ");  //salva o arquivo.
+			fclose(file);  //fecha o arquivo.
+			
+			file = fopen(arquivo, "a"); //"a" serve para editar um arquivo ja existente.
+			fprintf(file, cpf); 
+			fclose(file);
+			
+			file = fopen(arquivo, "a");
+			fprintf(file, "\n");
+			fclose(file);
+			  //aqui acaba o criação de um arquivo novo e o registro do cpf.
+			
+			  //NOME
+			file = fopen(arquivo, "a");
+			fprintf(file, "NOME:");
+			fclose(file);
+			
+			printf("digite o Nome a ser cadastrado: ");
+			scanf("%s", nome);
+		
+			file = fopen(arquivo, "a"); 
+			fprintf(file, nome);
+			fclose(file);
+			
+			file = fopen(arquivo, "a");
+			fprintf(file, "\n");
+			fclose(file);
+			  //registro do nome encerrado.
+			
+			  //SOBRENOME
+			file = fopen(arquivo, "a");
+			fprintf(file, "SOBRENOME: ");
+			fclose(file);
+			
+			printf("digite o Sobrenome a ser cadastrado: ");
+			scanf("%s", sobrenome);
+		
+			file = fopen(arquivo, "a");
+			fprintf(file, sobrenome);
+			fclose(file);
+			
+			file = fopen(arquivo, "a");
+			fprintf(file, "\n");
+			fclose(file);
+			  //registro do sobrenome encerrado.
+			
+			  //CARGO
+			file = fopen(arquivo, "a");
+			fprintf(file, "CARGO: ");
+			fclose(file);
+			
+			printf("digite o Cargo a ser cadastrado: ");
+			scanf("%s", cargo);
+		
+			file = fopen(arquivo, "a");
+			fprintf(file, cargo);
+			fclose(file);
+			
+			file = fopen(arquivo, "a");
+			fprintf(file, "\n");
+			fclose(file);
+			  //registro do cargo encerrado.
+			system("cls");
+			}	
+	  // Fim da criação de variáveis.
+	  
+	  //Criação de arquivo e novo registro do cpf.
 	printf("Digite o CPF a ser cadastrado: ");
 	scanf("%s", cpf);
 	
 	strcpy(arquivo, cpf);  //Copia os valores das strings. Como se fosse (aqruivo = cpf).
 	
-	FILE *file;  //Cria um arquivo
-	file = fopen(arquivo, "w");  //fopen abre o arquivo. "w" serve para criar um novo arquivo.
-	fprintf(file, "CPF: ");  //salva o arquivo.
-	fclose(file);  //fecha o arquivo.
-	
-	file = fopen(arquivo, "a"); //"a" serve para editar um arquivo ja existente.
-	fprintf(file, cpf); 
-	fclose(file);
-	
-	file = fopen(arquivo, "a");
-	fprintf(file, "\n");
-	fclose(file);
-	  //aqui acaba o criação de um arquivo novo e o registro do cpf.
-	
-	  //NOME
-	file = fopen(arquivo, "a");
-	fprintf(file, "NOME:");
-	fclose(file);
-	
-	printf("digite o Nome a ser cadastrado: ");
-	scanf("%s", nome);
-
-	file = fopen(arquivo, "a"); 
-	fprintf(file, nome);
-	fclose(file);
-	
-	file = fopen(arquivo, "a");
-	fprintf(file, "\n");
-	fclose(file);
-	  //registro do nome encerrado.
-	
-	  //SOBRENOME
-	file = fopen(arquivo, "a");
-	fprintf(file, "SOBRENOME: ");
-	fclose(file);
-	
-	printf("digite o Sobrenome a ser cadastrado: ");
-	scanf("%s", sobrenome);
-
-	file = fopen(arquivo, "a");
-	fprintf(file, sobrenome);
-	fclose(file);
-	
-	file = fopen(arquivo, "a");
-	fprintf(file, "\n");
-	fclose(file);
-	  //registro do sobrenome encerrado.
-	
-	  //CARGO
-	file = fopen(arquivo, "a");
-	fprintf(file, "CARGO: ");
-	fclose(file);
-	
-	printf("digite o Cargo a ser cadastrado: ");
-	scanf("%s", cargo);
-
-	file = fopen(arquivo, "a");
-	fprintf(file, cargo);
-	fclose(file);
-	
-	file = fopen(arquivo, "a");
-	fprintf(file, "\n");
-	fclose(file);
-	  //registro do cargo encerrado.
-	
 	system("cls");
-	printf("Usuario registrado!\n\n");
-	system("pause");
+	
+	FILE *file;
+	file = fopen(cpf, "r");
+	
+	if(file == NULL){
+		registrar();
+		
+		system("cls");
+		printf("Usuario registrado!\n\n");
+		system("pause");
+	}else{
+		printf("Esse arquivo ja existe!\n\n");
+		printf("Deseja edita-lo?\n\t1 - Sim!\n\t2 - Não!");
+		printf("opção: ");
+		scanf("%d",&opcao);
+		
+		system("cls");
+		
+		switch(opcao){
+			case 1:
+				printf("Editando arquivo do usuário: ");
+				printf(cpf);
+				printf("\n\n");
+				registrar();
+				
+				system("cls");
+				printf("Usuario editado!\n\n");
+				system("pause");
+			break;
+			
+			case 2:
+				printf("Cancelando registro...\n\n");
+				system("pause");
+			break;
+			
+			default:
+				printf("Opcao Invalida\n\n");
+				system("pause");	
+			break;	
+		}	
+	}
+	fclose(file);	
 }
 
 int consulta(){
 	setlocale (LC_ALL, "Portuguese");
-	
+	  
+	  //Criação de variáveis/strings.
 	char cpf[40];
 	char conteudo[300];
-	
+	  //fim da criação de variáveis.
+	  
+	  //consulta dos arquivos.
 	printf("Digite o cpf que deseja consultar: ");
 	scanf("%s", cpf);
 	
@@ -113,11 +161,69 @@ int consulta(){
 	printf("\n");
 	system("pause");
 	}
+	fclose(file);  //Não explicaram, mas acabei descobrindo depois de muita tentativa e erro, que para excluir um arquivo aberto, é obrigatório fecha-lo anteriormente.
+	  //fim da consulta.
 }
 	
 int deletar(){
-	printf("Você escolheu deletar arquivos.\n");
-	system("pause");
+	setlocale (LC_ALL, "Portuguese");
+	  
+	  //Criação de variáveis/strings/numbers.
+	char cpf [40];
+	char conteudo [300];
+	int opcao = 0;
+	  //fim da Criação de variáveis/strings/numbers.
+	  
+	  //etapa de consulta de cpf para exclusão.
+	printf("Insira o CPF a ser deletado: ");
+	scanf("%s",cpf);
+	
+	FILE *file;
+	file = fopen(cpf, "r");
+	system("cls");
+	
+	
+	if(file == NULL){
+		printf("Arquivo não localizado.\n\n");
+		system("pause");
+		//fim da consulta para exclusão.
+		
+		//etapa de confirmação da exclusão
+	}else{
+		system("cls");
+		
+		while(fgets(conteudo, 200, file) != NULL){  //fgets serve para buscar o arquivo. ele vai buscar o aqruivo conteudo, ate 200 caracteres enquanto o mesmo for diferente de NULL.
+		printf("%s", conteudo);
+		}
+		
+		fclose(file);  //tenho que fechar o arquivo para conseguir exvluir ele! 
+		
+		printf("\nDeseja excluir esse usuário? \n\n\t1 - SIM! \n\t2 - NAO!\n\n");
+		printf("Opcao: "); 
+		
+		scanf("%d",&opcao);  //armazena a resposta do usuário na variável. (tipo o prompt).
+		
+		system("cls");
+		
+		switch(opcao){
+			case 1:
+				remove(cpf);
+				printf("Usuario excluído com sucesso!\n\n");
+				system("pause");
+			break;
+			
+			case 2:
+				printf("Cancelando exclusão...\n\n");
+				system("pause");
+			break;
+			
+			default:
+				printf("Opção Inválida!");
+				system("pause");
+			break;	
+		      //fim da etapa de confirmação da exclusão.		
+		}	
+	}	
 }
 
 int main(){
@@ -134,7 +240,8 @@ int main(){
 		printf(" Selecione a opcao desejada no menu:\n\n");
 		printf("\t1 - Registrar Nome\n");  //\t é ultilizado para dar um espaçamento antes do texto.
 		printf("\t2 - Consultar Nomes\n");
-		printf("\t3 - Deletar Nome\n\n\n"); 
+		printf("\t3 - Deletar Nome\n"); 
+		printf("\t4 - Fechar Programa\n\n\n");
 		printf("Opcao: ");  //fim do menu
 		
 		scanf("%d",&opcao);  //armazena a resposta do usuário na variável. (tipo o prompt).
@@ -150,6 +257,10 @@ int main(){
 			break;	
 			case 3:
 				deletar();
+			break;
+			case 4:
+				printf("Encerrando o programa...\n\n");
+				return 0;   // retorno 0, então fecha o programa.
 			break;
 			default:
 				printf("Opcao invalida.\n\n");
